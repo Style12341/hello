f6926aceb5d6f5c61f8bd225903a8eede526fec6
Bernard Maximiliano <bmaximiliano0210@gmail.com>
2024-10-05 14:27:38 -0300

    context guide: categories

---
+++ lib/hello/catalog/product.ex
@@ -0,0 +1,24 @@
+defmodule Hello.Catalog.Product do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "products" do
+    field :description, :string
+    field :title, :string
+    field :price, :decimal
+    field :views, :integer
+
+    many_to_many :categories, Hello.Catalog.Category,
+      join_through: "product_categories",
+      on_replace: :delete
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(product, attrs) do
+    product
+    |> cast(attrs, [:title, :description, :price, :views])
+    |> validate_required([:title, :description, :price, :views])
+  end
+end
